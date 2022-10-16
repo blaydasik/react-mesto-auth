c23366a88c97e08b17e910b0e64659db297a22cd
Алексей Куров <alexei.s.kurov@gmail.com>
2022-10-17 02:53:30 +0300

    Updates

--- src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-    <footer className="footer">
-      <p className="footer__copyright">&copy; {new Date().getFullYear()} Алексей Куров</p>
-    </footer>
-  );
-}
-
-export default Footer;
